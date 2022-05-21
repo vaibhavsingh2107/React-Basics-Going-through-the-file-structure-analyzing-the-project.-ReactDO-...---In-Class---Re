966a901aed3461393f8d54d748b46d685d144fff
Vaibhav Singh <77732000+vaibhavsingh2107@users.noreply.github.com>
2022-05-21 12:49:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <h2 data-ns-test="project-name">Amazone Clone Website</h2>
+      <p data-ns-test="project-description">
+        This is an Amazon Clone Website with almost all features of Amazon built
+        using React.
+      </p>
+    </div>
+  );
+};
 
 export default App;
